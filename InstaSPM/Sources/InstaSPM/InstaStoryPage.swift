7f83389721a0dd79d733ccb667e06816a65c8d5b
MarcelC24 <108124259+MarcelC24@users.noreply.github.com>
2023-05-12 14:43:30 +0200

    Implement base Network impl

--- InstaSPM/Sources/InstaSPM/InstaStoryPage.swift
+++ InstaSPM/Sources/InstaSPM/InstaStoryPage.swift
@@ -1,14 +1,73 @@
 import SwiftUI
 
 public struct InstaStoryPage: View {
+    public init() {
+    }
+    @State private var isLoading = false
+    @State private var stories: [Story]?
+    
     public var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            if isLoading {
+                ProgressView()
+            } else {
+                Button(action: {
+                    fetchData(NetworkURLs.list2Pics)
+                }, label: {
+                    Text("Fetch 2 Pics")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                .padding()
+            }
+            if isLoading {
+                ProgressView()
+            } else {
+                Button(action: {
+                    fetchData(NetworkURLs.list10Pics)
+                }, label: {
+                    Text("Fetch 10 Pics")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                .padding()
+            }
+            if isLoading {
+                ProgressView()
+            } else {
+                Button(action: {
+                    fetchData(NetworkURLs.listVideos)
+                }, label: {
+                    Text("Fetch Videos")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                .padding()
+            }
         }
         .padding()
+    }
+    
+    func fetchData(_ urlString: String) {
+        isLoading = true
+        Task {
+            do {
+                stories = try await Network.fetchStories(urlString)
+            } catch {
+                print("Error: \(error)")
+            }
+            isLoading = false
+        }
     }
 }
 
---
+++ InstaSPM/Sources/InstaSPM/Network/Network.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+
+struct NetworkURLs {
+    static let list2Pics = "https://b13f05a4-376f-4dc0-962e-ca4b7bdc2af4.mock.pstmn.io/stories/list"
+    static let list10Pics = "https://b13f05a4-376f-4dc0-962e-ca4b7bdc2af4.mock.pstmn.io/stories/list"
+    static let listVideos = "https://b13f05a4-376f-4dc0-962e-ca4b7bdc2af4.mock.pstmn.io/stories/list"
+}
+
+struct Stories: Decodable {
+    let stories: [Story]
+}
+
+struct Story: Decodable {
+    
+    let id: Int // 125
+    let url: String // "https://wallpapers.com/images/featured-full/mobile-58g8gv3r23zg29kw.jpg",
+    let length: Int // 50,
+    let posted: Int // 1683885376,
+//          let banners: [],
+    let status: String // "SEEN"
+}
+
+class Network {
+    public static func fetchStories(_ urlString: String) async throws -> [Story] {
+        
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            throw URLError(URLError.Code(rawValue: -1337))
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        let stories = try decoder.decode(Stories.self, from: data)
+        print("Did Fetch \(stories.stories)")
+        return stories.stories
+    }
+}
